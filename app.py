51644f6bd072bcefb7286d7d6c8e47f559832e5f
Luiz <luiz.victor9973@gmail.com>
2025-07-21 13:44:20 -0300

    Feito a renderização da página sobre o projeto e sobre nós

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 def get_sinonimos(palavra):
     url = f"https://www.sinonimos.com.br/{palavra}/"
@@ -14,11 +15,14 @@ def get_sinonimos(palavra):
         p_syn_list = soup.find("p", class_="syn-list syn-list-1")
         if not p_syn_list:
             return []
-        sinonimos = [a.text.strip() for a in p_syn_list.find_all("a", class_="sinonimo")]
+        sinonimos = [
+            a.text.strip()
+            for a in p_syn_list.find_all("a", class_="sinonimo")]
         return sinonimos
     except Exception as e:
         print(f"Erro ao buscar sinônimos: {e}")
         return []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -29,6 +33,17 @@ def index():
         if palavra:
             sinonimos = get_sinonimos(palavra)
     return render_template('index.html', sinonimos=sinonimos, palavra=palavra)
+
+
+@app.route("/about-project/")
+def about_project():
+    return render_template("projeto.html")
+
+
+@app.route("/about-we/")
+def about_we():
+    return render_template("quem-somos-nos.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
